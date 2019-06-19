77813f1e9a587f47cd7faa809bf027cf74aa13b5
Aidan M <asmaidan@outlook.com>
2019-06-18 22:58:52 -0500

    added Dio

--- lib/yt2_audio.dart
+++ lib/yt2_audio.dart
@@ -1,11 +1,21 @@
 library yt2_audio;
+import 'dart:core';
+import 'dart:core';
 import 'package:youtube_extractor/youtube_extractor.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/foundation.dart';
+import 'package:dio/dio.dart';
 
 
 class YT2Music {
 
+  Dio dio = new Dio();
+
+  downLoad(String param) async {
+    var data = await extractor.getMediaStreamsAsync(param);
+    String url = data.audio.first.url;
+    await dio.download(url, "./test.mp3");    
+  }
 
   AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
   var extractor = YouTubeExtractor();
