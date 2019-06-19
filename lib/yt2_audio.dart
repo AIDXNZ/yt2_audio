cf3bd6e09201fc059da81d85c67125aed0d4645a
Aidan M <asmaidan@outlook.com>
2019-06-19 00:49:44 -0500

    bug fix

--- lib/yt2_audio.dart
+++ lib/yt2_audio.dart
@@ -1,20 +1,29 @@
 library yt2_audio;
 import 'dart:core';
 import 'dart:core';
+import 'dart:io';
 import 'package:youtube_extractor/youtube_extractor.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/foundation.dart';
 import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
 
 
 class YT2Music {
+
+  Future<String> get _localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+
+  return directory.path;
+}
+
 
   Dio dio = new Dio();
 
   downLoad(String param) async {
     var data = await extractor.getMediaStreamsAsync(param);
     String url = data.audio.first.url;
-    await dio.download(url, "./test.mp3");    
+    await dio.download(url, _localPath);    
   }
 
   AudioPlayer audioPlayer = AudioPlayer(mode: PlayerMode.LOW_LATENCY);
